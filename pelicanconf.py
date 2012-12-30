632d3a741a8dc5d0c4d46e7e6b55f912dfaccc9e
Xidorn Quan <quanxunzhen@gmail.com>
2012-12-30 23:53:14 +0800

    初始化 Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Xidorn Quan'
+SITENAME = u'\u9b3c\u306e\u9886\u5730'
+SITEURL = 'http://upsuper.github.com/blog'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
